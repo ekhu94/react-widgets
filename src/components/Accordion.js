6009a11f334322d8d4c825b409fe13d33a13dfa9
Erik Huang <erikhuangdrums@gmail.com>
2021-03-14 22:33:25 -0700

    assign active class based on activeIndex

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -8,13 +8,14 @@ const Accordion = ({ items }) => {
     };
 
     const renderedItems = items.map((item, i) => {
+        const active = i === activeIndex ? 'active' : '';
         return (
             <React.Fragment key={item.title}>
-                <div className="title active" onClick={() => onTitleClick(i)} >
+                <div className={`title ${active}`} onClick={() => onTitleClick(i)} >
                     <i className="dropdown icon"></i>
                     {item.title}
                 </div>
-                <div className="content active">
+                <div className={`content ${active}`}>
                     <p>{item.content}</p>
                 </div>
             </React.Fragment>
@@ -24,7 +25,6 @@ const Accordion = ({ items }) => {
     return (
         <div className="ui styled accordion">
             {renderedItems}
-            <h1>{activeIndex}</h1>
         </div>
     );
 };
