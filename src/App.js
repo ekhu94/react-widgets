41f121a51daa9ffce3e7f87e24a5d79e500c14f6
Erik Huang <erikhuangdrums@gmail.com>
2021-03-14 21:35:02 -0700

    create Accordion, pass items prop down from App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front-end JS library.'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favored library among engineers.'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'React is utilized through many components.'
+    }
+];
 
 const App = () => {
-    return <h1>Widgets App</h1>
+    return (
+        <Accordion items={items} />
+    );
 }
 
 export default App;
