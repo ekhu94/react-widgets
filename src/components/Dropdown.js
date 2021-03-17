372f0b0be280167fb826e4ee3687497fec3e2a10
Erik Huang <ekhu94@gmail.com>
2021-03-17 06:41:40 -0700

    filter out options based on selected

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const renderedOptions = options.map(o => {
+        if (o.value === selected.value) return null;
         return (
             <div 
                 key={o.value} 
