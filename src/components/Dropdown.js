b5ac496df0b7aa5365ea7754ce972b8698511fd3
Erik Huang <ekhu94@gmail.com>
2021-03-17 07:26:06 -0700

    toggle visibility with state of open

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+
     const renderedOptions = options.map(o => {
         if (o.value === selected.value) return null;
         return (
@@ -17,12 +19,17 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
         <div className="ui form">
             <div className="field">
                 <label className="label">Color Selector</label>
-                <div className="ui selection dropdown visible active">
+                <div 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                    onClick={() => setOpen(!open)}
+                >
                     <i className="dropdown icon"></i>
                     <div className="text">
                         {selected.label}
                     </div>
-                    <div className="menu visible transition">
+                    <div 
+                        className={`menu ${open ? 'visible transition' : ''}`}
+                    >
                         {renderedOptions}
                     </div>
                 </div>
