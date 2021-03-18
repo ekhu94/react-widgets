f0c3a32372d99f3998cc36cace3ef6002962faa0
Erik Huang <ekhu94@gmail.com>
2021-03-18 16:08:09 -0700

    make POST request with axios in useEffect

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ text, lang }) => {
     useEffect(() => {
+        const onTranslateChange = async () => {
+            const res = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: lang.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            });
+        }
         
     }, [text, lang])
 
