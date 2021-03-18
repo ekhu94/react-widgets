5258878a544b27312aade0b18e2cf1490c9159cb
Erik Huang <ekhu94@gmail.com>
2021-03-18 15:55:32 -0700

    add Convert component to Translate with useEffect wired

---
+++ src/components/Convert.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const Convert = ({ text, lang }) => {
+    useEffect(() => {
+        
+    }, [text, lang])
+
+    return (
+        <div></div>
+    );
+};
+
+export default Convert;
