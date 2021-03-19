14350bf96b283d8c3ddbd88f881b902be22ce9b7
Erik Huang <ekhu94@gmail.com>
2021-03-19 10:36:50 -0700

    add window.history.pushState function to click event

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Link = ({ href, className, children }) => {
     const onClick = e => {
         e.preventDefault();
+        window.history.pushState({}, '', href);
     };
     return (
         <a className={className} href={href} onClick={onClick}>
